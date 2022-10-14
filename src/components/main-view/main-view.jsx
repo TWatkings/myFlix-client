492225a9b7c0c15260bc3fb734534608d7fa8148
TWatkings <106554653+TWatkings@users.noreply.github.com>
2022-10-14 18:14:32 -0400

    add axios

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -7,11 +8,15 @@ export class MainView extends React.Component {
   constructor(){
     super();
     this.state = {
-      movies: [
-        { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
-        { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
-      ],
+      movies: [],
+      componentDidMount() {
+        axios.get('https://tomflix.herokuapp.com/movies')
+          .then(response => {
+            this.setState({
+              movies: response.date
+            });
+          })
+      },
       selectedMovie: null
     };
   }
@@ -24,7 +29,7 @@ export class MainView extends React.Component {
   render() {
     const { movies, selectedMovie } = this.state;
   
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    if (movies.length === 0) return <div className="main-view" />;
   
     return (
       <div className="main-view">
